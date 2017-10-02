9928f0234af4fa171eaaae2049ff42e528ea08d5
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2017-10-02 23:22:03 +0800

    Implement LDA imm instruction
    Signed-off-by: Ying-Ruei Liang(KK) <thumbd03803@gmail.com>

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,45 +1,72 @@
+use opcode;
 use opcode::OpCode;
 
 const MEM_ADDR_MAX: usize = 512;
 
-pub struct RP2A03 {
-    memory: [u8; MEM_ADDR_MAX],
+#[derive(Debug, Clone, PartialEq)]
+pub struct Registers {
+    pub a: u8,
+    pub x: u8,
+    pub y: u8,
+    pub p: u8,
+    pub pc: u8,
+    pub sp: u8,
+}
 
-    reg_a: u8,
-    reg_x: u8,
-    reg_y: u8,
-    reg_p: u8,
-    reg_pc: u8,
-    reg_sp: u8,
+#[derive(Debug, Clone, PartialEq)]
+pub struct Memory {
+    raw_memory: Vec<u8>,
+}
+
+impl Memory {
+    pub fn new() -> Memory {
+        Memory { raw_memory: vec![0x00; MEM_ADDR_MAX] }
+    }
+
+    pub fn read(&self, addr: u16) -> u8 {
+        self.raw_memory[addr as usize]
+    }
+
+    pub fn write(&mut self, addr: u16, val: u8) {
+        self.raw_memory[addr as usize] = val;
+    }
+}
+
+pub struct RP2A03 {
+    memory: Memory,
+    registers: Registers,
+    current_cycles: u32,
 }
 
 impl RP2A03 {
     pub fn new() -> RP2A03 {
         RP2A03 {
-            memory: [0x00; MEM_ADDR_MAX],
-
-            reg_a: 0,
-            reg_x: 0,
-            reg_y: 0,
-            reg_p: 0,
-            reg_pc: 0,
-            reg_sp: 0,
+            memory: Memory::new(),
+            registers: Registers {
+                a: 0x00,
+                x: 0x00,
+                y: 0x00,
+                p: 0x00,
+                pc: 0x00,
+                sp: 0x00,
+            },
+            current_cycles: 0,
         }
     }
 
     pub fn execute(&mut self) {
-        let pc = self.reg_pc;
+        let pc = self.registers.pc;
 
-        let opcode = OpCode::from(self.memory[pc as usize]);
-        let operands_num = opcode.operands_num();
-        let operands = &self.memory[(pc as usize)..((pc + operands_num) as usize)];
-
-        self.reg_pc += operands_num;
+        let opcode = OpCode::from(self.memory.read(pc as u16));
 
         match opcode {
-            OpCode::Nop => {}
+            OpCode::Nop => opcode::nop(&mut self.registers, &mut self.memory),
+            OpCode::LdaImm => opcode::lda_imm(&mut self.registers, &mut self.memory),
             _ => unimplemented!(),
         }
+
+        self.registers.pc += (1 + opcode.operands_num());
+        self.current_cycles += opcode.cycles_num() as u32;
     }
 }
 
@@ -51,25 +78,40 @@ mod test {
         use super::*;
 
         #[test]
-        fn nop() {
+        fn lda_imm() {
             let mut cpu = RP2A03::new();
-            cpu.memory[0] = OpCode::Nop.into();
+            cpu.memory.write(0, OpCode::LdaImm.into());
+            cpu.memory.write(1, 0x42);
 
-            let old_mem = cpu.memory;
-            let old_reg_a = cpu.reg_a;
-            let old_reg_p = cpu.reg_p;
-            let old_reg_sp = cpu.reg_sp;
-            let old_reg_x = cpu.reg_x;
-            let old_reg_y = cpu.reg_y;
+            let mem_snapshot = cpu.memory.clone();
+            let regs_snaptshot = cpu.registers.clone();
 
             cpu.execute();
 
-            assert_eq!(cpu.memory.as_ref(), old_mem.as_ref());
-            assert_eq!(cpu.reg_a, old_reg_a);
-            assert_eq!(cpu.reg_p, old_reg_p);
-            assert_eq!(cpu.reg_sp, old_reg_sp);
-            assert_eq!(cpu.reg_x, old_reg_x);
-            assert_eq!(cpu.reg_y, old_reg_y);
+            assert_eq!(cpu.memory, mem_snapshot);
+            assert_eq!(cpu.registers.a, 0x42);
+            assert_eq!(cpu.registers.p, regs_snaptshot.p);
+            assert_eq!(cpu.registers.sp, regs_snaptshot.sp);
+            assert_eq!(cpu.registers.x, regs_snaptshot.x);
+            assert_eq!(cpu.registers.y, regs_snaptshot.y);
+        }
+
+        #[test]
+        fn nop() {
+            let mut cpu = RP2A03::new();
+            cpu.memory.write(0, OpCode::Nop.into());
+
+            let mem_snapshot = cpu.memory.clone();
+            let regs_snaptshot = cpu.registers.clone();
+
+            cpu.execute();
+
+            assert_eq!(cpu.memory, mem_snapshot);
+            assert_eq!(cpu.registers.a, regs_snaptshot.a);
+            assert_eq!(cpu.registers.p, regs_snaptshot.p);
+            assert_eq!(cpu.registers.sp, regs_snaptshot.sp);
+            assert_eq!(cpu.registers.x, regs_snaptshot.x);
+            assert_eq!(cpu.registers.y, regs_snaptshot.y);
         }
     }
 }
--- src/opcode.rs
+++ src/opcode.rs
@@ -1,12 +1,25 @@
 use std::convert::From;
 
+use cpu::Registers;
+use cpu::Memory;
+
+pub fn nop(_registers: &mut Registers, _mem: &mut Memory) {}
+pub fn lda_imm(registers: &mut Registers, mem: &mut Memory) {
+    let pc = &registers.pc;
+    let val = mem.read((pc + 1) as u16);
+
+    registers.a = val;
+}
+
 pub enum OpCode {
     Nop,
+    LdaImm,
 }
 
 impl OpCode {
     pub fn operands_num(&self) -> u8 {
         match self {
+            &OpCode::LdaImm => 1,
             &OpCode::Nop => 0,
             _ => unimplemented!(),
         }
@@ -14,6 +27,7 @@ impl OpCode {
 
     pub fn cycles_num(&self) -> u8 {
         match self {
+            &OpCode::LdaImm => 2,
             &OpCode::Nop => 2,
             _ => unimplemented!(),
         }
@@ -23,6 +37,7 @@ impl OpCode {
 impl From<u8> for OpCode {
     fn from(byte: u8) -> OpCode {
         match byte {
+            0xA9 => OpCode::LdaImm,
             0xEA => OpCode::Nop,
             _ => unimplemented!(),
         }
@@ -32,6 +47,7 @@ impl From<u8> for OpCode {
 impl Into<u8> for OpCode {
     fn into(self) -> u8 {
         match self {
+            OpCode::LdaImm => 0xA9,
             OpCode::Nop => 0xEA,
             _ => unimplemented!(),
         }

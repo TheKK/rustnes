8afb3b52fdcec567821b56b4c43cee2bb77ce949
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2017-10-03 19:28:22 +0800

    Use macro to reduce duplicated code
    Signed-off-by: Ying-Ruei Liang(KK) <thumbd03803@gmail.com>

--- src/cpu.rs
+++ src/cpu.rs
@@ -74,88 +74,61 @@ mod test {
     mod opcode {
         use super::*;
 
-        #[test]
-        fn lda_imm() {
-            let mut cpu = RP2A03::new();
-            cpu.memory.write(0, OpCode::LdaImm.into());
+        fn imm(cpu: &mut RP2A03) {
             cpu.memory.write(1, 0x42);
-
-            let mem_snapshot = cpu.memory.clone();
-            let regs_snaptshot = cpu.registers.clone();
-
-            cpu.execute();
-
-            assert_eq!(cpu.memory, mem_snapshot);
-            assert_eq!(cpu.registers.a, 0x42);
-            assert_eq!(cpu.registers.p, regs_snaptshot.p);
-            assert_eq!(cpu.registers.sp, regs_snaptshot.sp);
-            assert_eq!(cpu.registers.x, regs_snaptshot.x);
-            assert_eq!(cpu.registers.y, regs_snaptshot.y);
         }
 
-        #[test]
-        fn lda_zero_page() {
-            let mut cpu = RP2A03::new();
-            cpu.memory.write(0, OpCode::LdaZeroPage.into());
+        fn zero_page(cpu: &mut RP2A03) {
             cpu.memory.write(1, 0x02);
             cpu.memory.write(2, 0x42);
-
-            let mem_snapshot = cpu.memory.clone();
-            let regs_snaptshot = cpu.registers.clone();
-
-            cpu.execute();
-
-            assert_eq!(cpu.memory, mem_snapshot);
-            assert_eq!(cpu.registers.a, 0x42);
-            assert_eq!(cpu.registers.p, regs_snaptshot.p);
-            assert_eq!(cpu.registers.sp, regs_snaptshot.sp);
-            assert_eq!(cpu.registers.x, regs_snaptshot.x);
-            assert_eq!(cpu.registers.y, regs_snaptshot.y);
         }
 
-        #[test]
-        fn lda_zero_page_x() {
-            let mut cpu = RP2A03::new();
-            cpu.memory.write(0, OpCode::LdaZeroPageX.into());
+        fn zero_page_x(cpu: &mut RP2A03) {
             cpu.memory.write(1, 0x00);
             cpu.memory.write(2, 0x42);
 
             cpu.registers.x = 0x02;
-
-            let mem_snapshot = cpu.memory.clone();
-            let regs_snaptshot = cpu.registers.clone();
-
-            cpu.execute();
-
-            assert_eq!(cpu.memory, mem_snapshot);
-            assert_eq!(cpu.registers.a, 0x42);
-            assert_eq!(cpu.registers.p, regs_snaptshot.p);
-            assert_eq!(cpu.registers.sp, regs_snaptshot.sp);
-            assert_eq!(cpu.registers.x, regs_snaptshot.x);
-            assert_eq!(cpu.registers.y, regs_snaptshot.y);
         }
 
-        #[test]
-        fn lda_abs() {
-            let mut cpu = RP2A03::new();
-            cpu.memory.write(0, OpCode::LdaAbs.into());
-            // LDA $0102, note the order.
+        fn abs(cpu: &mut RP2A03) {
+            // ins $0102, note the order.
             cpu.memory.write(1, 0x02);
             cpu.memory.write(2, 0x01);
             cpu.memory.write(0x0102, 0x42);
-
-            let mem_snapshot = cpu.memory.clone();
-            let regs_snaptshot = cpu.registers.clone();
-
-            cpu.execute();
-
-            assert_eq!(cpu.memory, mem_snapshot);
-            assert_eq!(cpu.registers.a, 0x42);
-            assert_eq!(cpu.registers.p, regs_snaptshot.p);
-            assert_eq!(cpu.registers.sp, regs_snaptshot.sp);
-            assert_eq!(cpu.registers.x, regs_snaptshot.x);
-            assert_eq!(cpu.registers.y, regs_snaptshot.y);
         }
+
+        macro_rules! assert_field_eq (
+            ($left: expr, $right: expr, [$($field: ident), *]) => {
+                $(
+                    assert_eq!($left.$field, $right.$field);
+                )*
+            }
+        );
+
+        macro_rules! lda_test (
+            ($test_name: ident, $opcode: expr, $arrange_fn: expr) => {
+                #[test]
+                fn $test_name() {
+                    let mut cpu = RP2A03::new();
+                    cpu.memory.write(0, $opcode.into());
+                    $arrange_fn(&mut cpu);
+
+                    let mem_snapshot = cpu.memory.clone();
+                    let regs_snaptshot = cpu.registers.clone();
+
+                    cpu.execute();
+
+                    assert_eq!(cpu.memory, mem_snapshot);
+                    assert_eq!(cpu.registers.a, 0x42);
+                    assert_field_eq!(cpu.registers, regs_snaptshot, [p, sp, x, y]);
+                }
+            }
+        );
+
+        lda_test!(lda_imm, OpCode::LdaImm, imm);
+        lda_test!(lda_zero_page, OpCode::LdaZeroPage, zero_page);
+        lda_test!(lda_zero_page_x, OpCode::LdaZeroPageX, zero_page_x);
+        lda_test!(lda_abs, OpCode::LdaAbs, abs);
 
         #[test]
         fn nop() {
@@ -168,11 +141,7 @@ mod test {
             cpu.execute();
 
             assert_eq!(cpu.memory, mem_snapshot);
-            assert_eq!(cpu.registers.a, regs_snaptshot.a);
-            assert_eq!(cpu.registers.p, regs_snaptshot.p);
-            assert_eq!(cpu.registers.sp, regs_snaptshot.sp);
-            assert_eq!(cpu.registers.x, regs_snaptshot.x);
-            assert_eq!(cpu.registers.y, regs_snaptshot.y);
+            assert_field_eq!(cpu.registers, regs_snaptshot, [a, p, sp, x, y]);
         }
     }
 }

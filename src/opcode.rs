c37720e50b9c176cf70a0166042cc4427500bbbe
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2017-10-03 20:11:52 +0800

    Applying macro to reduce duplicated code
    Signed-off-by: Ying-Ruei Liang(KK) <thumbd03803@gmail.com>

--- src/opcode.rs
+++ src/opcode.rs
@@ -3,6 +3,71 @@ use std::convert::From;
 use cpu::Registers;
 use cpu::Memory;
 
+macro_rules! opcodes {
+    (
+        $(
+            ($opcode: ident,
+             $opcode_hex: expr,
+             $operands_size: expr,
+             $cycles_num: expr,
+             $opcode_fn: expr)
+        ),*
+    ) => {
+        pub enum OpCode {
+            $(
+               $opcode,
+            )*
+        }
+
+        impl OpCode {
+            pub fn operands_num(&self) -> u8 {
+                match self {
+                    $(
+                        &OpCode::$opcode => $operands_size,
+                    )*
+                }
+            }
+
+            pub fn cycles_num(&self) -> u8 {
+                match self {
+                    $(
+                        &OpCode::$opcode => $cycles_num,
+                    )*
+                }
+            }
+
+            pub fn get_fn(&self) -> fn(&mut Registers, &mut Memory) {
+                match self {
+                    $(
+                        &OpCode::$opcode => $opcode_fn,
+                    )*
+                }
+            }
+        }
+
+        impl From<u8> for OpCode {
+            fn from(byte: u8) -> OpCode {
+                match byte {
+                    $(
+                        $opcode_hex => OpCode::$opcode,
+                    )*
+                    _ => unimplemented!(),
+                }
+            }
+        }
+
+        impl Into<u8> for OpCode {
+            fn into(self) -> u8 {
+                match self {
+                    $(
+                        OpCode::$opcode => $opcode_hex,
+                    )*
+                }
+            }
+        }
+    }
+}
+
 fn nop(_registers: &mut Registers, _mem: &mut Memory) {}
 
 fn lda_imm(registers: &mut Registers, mem: &mut Memory) {
@@ -36,75 +101,10 @@ fn lda_abs(registers: &mut Registers, mem: &mut Memory) {
     registers.a = mem.read(addr);
 }
 
-pub enum OpCode {
-    Nop,
-    LdaImm,
-    LdaZeroPage,
-    LdaZeroPageX,
-    LdaAbs,
-}
-
-impl OpCode {
-    pub fn operands_num(&self) -> u8 {
-        match self {
-            &OpCode::LdaImm => 1,
-            &OpCode::LdaZeroPage => 1,
-            &OpCode::LdaZeroPageX => 1,
-            &OpCode::LdaAbs => 2,
-            &OpCode::Nop => 0,
-        }
-    }
-
-    pub fn cycles_num(&self) -> u8 {
-        match self {
-            &OpCode::LdaImm => 2,
-            &OpCode::LdaZeroPage => 3,
-            &OpCode::LdaZeroPageX => 4,
-            &OpCode::LdaAbs => 4,
-            &OpCode::Nop => 2,
-        }
-    }
-
-    pub fn get_fn(&self) -> fn(&mut Registers, &mut Memory) {
-        match self {
-            &OpCode::LdaImm => lda_imm,
-            &OpCode::LdaZeroPage => lda_zero_page,
-            &OpCode::LdaZeroPageX => lda_zero_page_x,
-            &OpCode::LdaAbs => lda_abs,
-            &OpCode::Nop => nop,
-        }
-    }
-}
-
-macro_rules! impl_from_and_into {
-    ($($opcode: path => $byte: expr), *) => (
-        impl From<u8> for OpCode {
-            fn from(byte: u8) -> OpCode {
-                match byte {
-                    $(
-                        $byte => $opcode,
-                    )*
-                    _ => unimplemented!(),
-                }
-            }
-        }
-
-        impl Into<u8> for OpCode {
-            fn into(self) -> u8 {
-                match self {
-                    $(
-                        $opcode => $byte,
-                    )*
-                }
-            }
-        }
-    )
-}
-
-impl_from_and_into! {
-    OpCode::LdaImm => 0xA9,
-    OpCode::LdaZeroPage => 0xA5,
-    OpCode::LdaZeroPageX => 0xB5,
-    OpCode::LdaAbs => 0xAD,
-    OpCode::Nop => 0xEA
-}
+opcodes!(
+    (LdaImm, 0xA9, 1, 2, lda_imm),
+    (LdaZeroPage, 0xA5, 1, 3, lda_zero_page),
+    (LdaZeroPageX, 0xB5, 1, 4, lda_zero_page_x),
+    (LdaAbs, 0xAD, 2, 4, lda_abs),
+    (Nop, 0xEA, 0, 2, nop)
+);

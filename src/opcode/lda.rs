c8d15b2711ea1ee9e5b5002f2e7132fbbfd0aeff
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2017-10-06 23:42:41 +0800

    Move address composing functions to opcode::utils
    Signed-off-by: Ying-Ruei Liang(KK) <thumbd03803@gmail.com>

--- src/opcode/lda.rs
+++ src/opcode/lda.rs
@@ -1,6 +1,9 @@
 use super::Cycle;
 use super::OpCode;
 
+use opcode::utils::compose_addr;
+use opcode::utils::compose_indexed_addr;
+
 use cpu::Registers;
 use cpu::Memory;
 
@@ -12,25 +15,6 @@ fn lda_assign_register_a(registers: &mut Registers, val: u8) {
     registers.a = val;
     registers.set_zero_flag(zero_flag);
     registers.set_sign_flag(sign_flag);
-}
-
-#[inline]
-fn compose_addr(addr_high: u8, addr_low: u8) -> u16 {
-    ((addr_high as u16) << 8) + addr_low as u16
-}
-
-#[inline]
-fn compose_indexed_addr(addr_high: u8, addr_low: u8, index: u8) -> (u16, bool) {
-    let (addr, page_crossed) = match addr_low.overflowing_add(index) {
-        (addr_low, true) => {
-            let (addr_high, _overflowed) = addr_high.overflowing_add(1);
-
-            (compose_addr(addr_high, addr_low), true)
-        }
-        (addr_low, false) => (compose_addr(addr_high, addr_low), false),
-    };
-
-    (addr, page_crossed)
 }
 
 pub fn lda_imm(registers: &mut Registers, mem: &mut Memory) -> Cycle {
--- src/opcode/utils.rs
+++ src/opcode/utils.rs
@@ -6,3 +6,22 @@ macro_rules! assert_field_eq (
         )*
     }
 );
+
+#[inline]
+pub fn compose_addr(addr_high: u8, addr_low: u8) -> u16 {
+    ((addr_high as u16) << 8) + addr_low as u16
+}
+
+#[inline]
+pub fn compose_indexed_addr(addr_high: u8, addr_low: u8, index: u8) -> (u16, bool) {
+    let (addr, page_crossed) = match addr_low.overflowing_add(index) {
+        (addr_low, true) => {
+            let (addr_high, _overflowed) = addr_high.overflowing_add(1);
+
+            (compose_addr(addr_high, addr_low), true)
+        }
+        (addr_low, false) => (compose_addr(addr_high, addr_low), false),
+    };
+
+    (addr, page_crossed)
+}
